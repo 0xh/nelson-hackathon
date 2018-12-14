8ee60057fdf3a93a0bc01f72d9b6cac58b103c8d
conradj <conradsjackson@gmail.com>
2018-12-14 09:20:53 +0000

    use nelson gql schema for position

--- store/index.js
+++ store/index.js
@@ -28,8 +28,8 @@ export const getters = {
 
 export const mutations = {
   UPDATE_CURRENT_POSITION(state, latlng) {
-    state.currentPosition.lat = latlng.lat;
-    state.currentPosition.lng = latlng.lng;
+    state.currentPosition.lat = latlng.latitude;
+    state.currentPosition.lng = latlng.longitude;
   },
   UPDATE_VESSEL_POSITIONS(state, positions) {
     state.vesselPositions = positions;
